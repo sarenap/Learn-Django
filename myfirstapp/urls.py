3d667afe698a1d12ad6d0205c0349ff4a5ed09a5
sarenap <122493371+sarenap@users.noreply.github.com>
2023-10-04 20:56:42 -0700

    django

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -6,16 +6,19 @@ urlpatterns = [
     path('about',views.myfunctionabout,name="about"),
     path('add/<int:a>/<int:b>',views.add,name="add"),
     path('intro/<str:name>/<int:age>',views.intro,name="intro"),
+
+    #binary string to decimal website converter.
+    path('decToBase/<int:num>/<int:base>', views.decToBase, name="decToBase"),
+
     path('myfirstpage',views.myfirstpage,name='myfirstpage'),
     path('mysecondpage',views.mysecondpage,name='mysecondpage'),
     path('mythirdpage',views.mythirdpage,name='mythirdpage'),
-    path('myimagepage',views.myimagepage,name='myimagepage'),
-    path('myimagepage2',views.myimagepage2,name='myimagepage2'),
-    path('myimagepage3',views.myimagepage3,name='myimagepage4'),
-    path('myimagepage4',views.myimagepage4,name='myimagepage4'),
-    path('myimagepage5/<str:imagename>',views.myimagepage5,name='myimagepage5'),
-    path('myform',views.myform,name='myform'),
-    path('submitmyform',views.submitmyform,name='submitmyform'),
-    path('myform2',views.myform2,name='myform2')
-   
+    path('myimagepage', views.myimagepage, name='myimagepage'), #views. call that function
+    path('myimagepage2', views.myimagepage2, name='myimagepage2'),
+    path('imagepage3', views.imagepage3, name='imagepage3'),
+    path('imagepage4', views.imagepage4, name='imagepage4'),
+    path('imagepage5/<str:imagename>', views.imagepage5, name='imagepage5'),
+    path('myform', views.myform, name='myform'),
+    path('submitmyform', views.submitmyform, name='submitmyform'),
+    path('myform2', views.myform2, name='myform2'),
 ]
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from .forms import *
+from .forms import * #import anyform
 # Create your views here.
 def error_404_view(request,exception):
-    return render(request,'404.html')
+    return render(request,'404.html') #rickrolll
 
 def myfunctioncall(request):
     return HttpResponse("Hello World")
@@ -17,67 +17,84 @@ def add(request,a,b):
 def intro(request,name,age):
     mydictionary = {
         "name" : name,
-        "age" : age
+        "age" : age,
     }
     return JsonResponse(mydictionary)
+
+def decToBase(request, num, base): #my own convert a dec to any base
+    #handle error
+    orignum = num
+    remainders = [] #empty list of remainders. 
+    while( num > 0 ):
+        rem = num % base
+        remainders.insert(0,rem) #prepend
+        num = num // base #floor division, round down to nearest whole num
+    mydictionary = {
+        "orignum" : orignum,
+        "num" : num,
+        "base" : base,
+        "remainders": remainders,
+    }
+    return render(request, 'bindec.html', context=mydictionary)
 
 def myfirstpage(request):
     return render(request,'index.html')
 
-def mysecondpage(request):
+def mysecondpage(request): 
     return render(request,'second.html')
 
 def mythirdpage(request):
-    var = "hello world"
+    #boolean. back end logic. 
+    #render to display on front end
+    var = "hello world" 
     greeting = "hey how are you"
-    fruits = ["apple","mango","banana"]
-    num1 , num2 = 10 , 7
+    fruits = ["dragonfruit", "mango", "grape"] #list
+    num1, num2 = 3, 5 
     ans = num1 > num2
-    #print(ans)
-    mydictionary = {
+    print(ans)
+
+    mydictionary = { #left: pass in to 3rd.html header
         "var" : var,
-        "msg" : greeting,
+        "msg" : greeting ,
         "myfruits" : fruits,
         "num1" : num1,
         "num2" : num2,
-        "ans" : ans
+        "ans" : ans,
     }
-    return render(request,'third.html',context=mydictionary)
+    return render(request, 'third.html' , context=mydictionary)
 
 def myimagepage(request):
-    return render(request,'imagepage.html')
-
+    return render(request, 'imagepage.html')
 
 def myimagepage2(request):
-    return render(request,'imagepage2.html')
+    return render(request, 'myimagepage2.html')
 
-def myimagepage3(request):
-    return render(request,'imagepage3.html')
+def imagepage3(request):
+    return render(request, 'imagepage3.html')
 
-def myimagepage4(request):
-    return render(request,'imagepage4.html')
+def imagepage4(request):
+    return render(request, 'imagepage4.html')
 
-def myimagepage5(request,imagename):
-    myimagename = str(imagename)
-    myimagename = myimagename.lower()
+def imagepage5(request, imagename):
+    myimagename = str(imagename);
+    myimagename = myimagename.lower();
     print(myimagename)
-    if myimagename == "django":
-        var=True
-    elif myimagename == "python":
-        var=False
-    mydictionary ={
-        "var":var
+    if myimagename == "django": var = True
+    elif myimagename == "python" : var = False
+
+    mydictionary = {
+        "var" : var,
     }
-    return render(request,'imagepage5.html',context=mydictionary)
+    return render(request, 'imagepage5.html', context = mydictionary)
 
 def myform(request):
-    return render(request,'myform.html')
+    return render(request, 'myform.html')
 
 def submitmyform(request):
     mydictionary = {
-        "var1" : request.POST['mytext'],
-        "var2" : request.POST['mytextarea'],
-        "method" : request.method
+        "var1": request.POST['mytext'],
+        "var2": request.POST['mytextarea'],
+        "method": request.method,
     }
     return JsonResponse(mydictionary)
 
@@ -98,7 +115,7 @@ def myform2(request):
                 errormsg = "Title should be in Capital"
                 Errors.append(errormsg)
             import re
-            regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+            regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$' #define email @
             if not re.search(regex,email):
                 errorflag = True
                 errormsg = "Not a Valid Email address"
@@ -111,14 +128,11 @@ def myform2(request):
             print(mydictionary)
             return render(request,'myform2.html',context=mydictionary)
 
-
-
-
-
-
     elif request.method == "GET":
         form = FeedbackForm() # FeedbackForm(None)
         mydictionary = {
             "form" : form
         }
         return render(request,'myform2.html',context=mydictionary)
+    
+    return HttpResponse("Invalid request") 
